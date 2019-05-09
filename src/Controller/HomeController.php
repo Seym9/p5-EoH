045f66c6e252287b225921c50b87964b24d8844e
sey_9 <sey_91360@hotmail.fr>
2019-05-09 15:20:57 +0200

    Creation of the home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
+use App\Repository\ArticlesRepository;
+use App\Repository\TopicsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
+     * @return Response
      */
-    public function index()
+    public function home(ArticlesRepository $repo, TopicsRepository $topic)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $articles = $repo->findOneBy(array(), array('id' => 'DESC'));
+        $topics = $topic->findByThree();
+
+        return $this->render('home/home.html.twig', [
+            'topics' => $topics,
+            'articles' => $articles,
+            'controller_name' => 'HomeController'
         ]);
     }
 }
