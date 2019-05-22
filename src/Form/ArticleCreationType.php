188e68f11b75c191cf18a4cd22b353379bcb1f0f
sey_9 <sey_91360@hotmail.fr>
2019-05-22 19:29:18 +0200

    Creation of forms for the creation of topic and the comments inside topics

---
+++ src/Form/ArticleCreationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use App\Entity\ArticlesCategories;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleCreationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('category', EntityType::class, [
+                'class' => ArticlesCategories::class,
+                'choice_label' => 'name'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
---
+++ src/Form/TopicType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\ForumCategories;
+use App\Entity\Topics;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TopicType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('category', EntityType::class, [
+                'class' => ForumCategories::class,
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Topics::class,
+        ]);
+    }
+}
