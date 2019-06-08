0ca869c3141a014f9a509ec2c965df5218fb24f5
sey_9 <sey_91360@hotmail.fr>
2019-06-08 12:19:56 +0200

    Adding ajax to reports of comments in articles and the forum

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,7 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\ArticleCommentReport;
+use App\Entity\ArticlesComments;
+use App\Entity\TopicCommentReport;
+use App\Entity\TopicReport;
+use App\Entity\Topics;
+use App\Entity\TopicsComments;
+use App\Repository\ArticleCommentReportRepository;
+use App\Repository\TopicCommentReportRepository;
+use App\Repository\TopicReportRepository;
+use DateTime;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
@@ -15,5 +27,86 @@ class CommentController extends AbstractController
         return $this->render('comment/index.html.twig', [
             'controller_name' => 'CommentController',
         ]);
+    }
+    /**
+     * Allow to report topics
+     *
+     * @Route ("/comment-topic/{id}/report", name="report_topic_comment")
+     *
+     * @param Topics $topic
+     * @param ObjectManager $manager
+     * @param TopicReportRepository $reportRepository
+     * @return Response
+     * @throws \Exception
+     */
+    public function reportTopicComment(TopicsComments $topicsComments, ObjectManager $manager, TopicCommentReportRepository $reportRepository){
+        $user = $this->getUser();
+
+        if (!$user) return $this->json([
+            'code' => 403,
+            'message' => "Unauthorized"
+        ], 403);
+
+        if ($topicsComments->isReportedByUser($user)){
+            return $this->json([
+                'code' => 403,
+                'message' => 'Already reported',
+            ], 403);
+        }
+
+        $report = new TopicCommentReport();
+        $report
+            ->setUser($user)
+            ->setComment($topicsComments)
+            ->setCreatedAt( new DateTime());
+
+        $manager->persist($report);
+        $manager->flush();
+        return $this->json([
+            'code' => 200,
+            'message' => 'Reported',
+            'report' => $reportRepository->count(['topic' => $topicsComments])
+        ], 200);
+    }
+
+    /**
+     * Allow to report topics
+     *
+     * @Route ("/comment-article/{id}/report", name="report_article_comment")
+     *
+     * @param Topics $topic
+     * @param ObjectManager $manager
+     * @param TopicReportRepository $reportRepository
+     * @return Response
+     * @throws \Exception
+     */
+    public function reportArticleComment(ArticlesComments $articlesComments, ObjectManager $manager, ArticleCommentReportRepository $reportRepository){
+        $user = $this->getUser();
+
+        if (!$user) return $this->json([
+            'code' => 403,
+            'message' => "Unauthorized"
+        ], 403);
+
+        if ($articlesComments->isReportedByUser($user)){
+            return $this->json([
+                'code' => 403,
+                'message' => 'Already reported',
+            ], 403);
+        }
+
+        $report = new ArticleCommentReport();
+        $report
+            ->setUser($user)
+            ->setComment($articlesComments)
+            ->setCreatedAt( new DateTime());
+
+        $manager->persist($report);
+        $manager->flush();
+        return $this->json([
+            'code' => 200,
+            'message' => 'Reported',
+            'report' => $reportRepository->count(['topic' => $articlesComments])
+        ], 200);
     }
 }
